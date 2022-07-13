0580e5162692bb67de0d137ab957d7cd4c5ac7e1
William Tran <cuong.tran@sotatek.com>
2022-07-13 11:15:16 +0700

    feat: mock oracle

---
+++ deployments/migrations/004_deploy_mock_oracle.ts
@@ -0,0 +1,16 @@
+import { DeployFunction } from "hardhat-deploy/dist/types";
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+
+const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment): Promise<void> {
+  const { deployments, getNamedAccounts } = hre;
+  const { deploy } = deployments;
+  const { deployer } = await getNamedAccounts();
+
+  await deploy("MockOracle", {
+    from: deployer,
+    log: true,
+  });
+};
+
+func.tags = ["MockOracle"];
+export default func;
