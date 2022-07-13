f9dd336e7eb3ad306f9a9bb519639aa300cdb552
William Tran <cuong.tran@sotatek.com>
2022-07-13 10:36:26 +0700

    feat: payment

--- contracts/Payment.sol
+++ contracts/Payment.sol
@@ -6,6 +6,7 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts-upgradeable/security/PausableUpgradeable.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
 contract Payment is Initializable, OwnableUpgradeable, PausableUpgradeable {
     enum PaymentType {
@@ -15,8 +16,11 @@ contract Payment is Initializable, OwnableUpgradeable, PausableUpgradeable {
     }
 
     address public treasury;
+    address public XCNToken;
+    address public usdtEthPriceFeed;
+    address public usdtXcnPriceFeed;
     // type + token => amount
-    mapping(PaymentType => mapping(address => uint256)) public payAmount;
+    mapping(PaymentType => uint256) public paymentAmountInUSDT;
     mapping(PaymentType => mapping(address => uint256)) public discount; // unit 1e18
     uint256 public constant HUNDRED_PERCENT = 1e18;
 
@@ -28,13 +32,22 @@ contract Payment is Initializable, OwnableUpgradeable, PausableUpgradeable {
         PaymentType paymentType,
         uint256 paymentId
     );
-    event SetPayAmount(PaymentType paymentType, address token, uint256 amount);
+    event SetPaymentAmount(PaymentType paymentType, uint256 amount);
     event SetDiscount(PaymentType paymentType, address token, uint256 discount);
     event ChangeTreasury(address treasury);
+    event SetOracle(address usdtEthPriceFeed, address usdtXcnPriceFeed);
 
-    function initialize(address _treasury) external initializer {
+    function initialize(
+        address _treasury,
+        address _XCN,
+        address _usdtEthPriceFeed,
+        address _usdtXcnPriceFeed
+    ) external initializer {
         __Ownable_init();
         treasury = _treasury;
+        XCNToken = _XCN;
+        usdtEthPriceFeed = 0x8A753747A1Fa494EC906cE90E9f37563A8AF630e;
+        usdtXcnPriceFeed = 0x8A753747A1Fa494EC906cE90E9f37563A8AF630e;
     }
 
     /**
@@ -51,13 +64,16 @@ contract Payment is Initializable, OwnableUpgradeable, PausableUpgradeable {
         _unpause();
     }
 
-    function setPayAmount(
-        PaymentType _type,
-        address _token,
-        uint256 _amount
-    ) external onlyOwner {
-        payAmount[_type][_token] = _amount;
-        emit SetPayAmount(_type, _token, _amount);
+    function setOracle(address _usdtEthPriceFeed, address _usdtXcnPriceFeed) external onlyOwner {
+        usdtEthPriceFeed = _usdtEthPriceFeed;
+        usdtXcnPriceFeed = _usdtXcnPriceFeed;
+
+        emit SetOracle(_usdtEthPriceFeed, _usdtXcnPriceFeed);
+    }
+
+    function setPaymentAmount(PaymentType _type, uint256 _amount) external onlyOwner {
+        paymentAmountInUSDT[_type] = _amount;
+        emit SetPaymentAmount(_type, _amount);
     }
 
     function setDiscount(
@@ -69,8 +85,8 @@ contract Payment is Initializable, OwnableUpgradeable, PausableUpgradeable {
         emit SetDiscount(_type, _token, _discount);
     }
 
-    function getPayAmount(PaymentType _type, address _token) external returns (uint256) {
-        return payAmount[_type][_token];
+    function getPaymentAmount(PaymentType _type) external returns (uint256) {
+        return paymentAmountInUSDT[_type];
     }
 
     function getDiscountAmount(PaymentType _type, address _token) external returns (uint256) {
@@ -83,26 +99,60 @@ contract Payment is Initializable, OwnableUpgradeable, PausableUpgradeable {
         uint256 _paymentId
     ) external payable {
         if (_token == address(0)) {
-            require(
-                msg.value ==
-                    payAmount[_type][address(0)] -
-                        (payAmount[_type][address(0)] * discount[_type][address(0)]) /
-                        HUNDRED_PERCENT,
-                "Payment: not valid pay amount"
-            );
-        } else {
-            IERC20(_token).transferFrom(
-                msg.sender,
-                treasury,
-                payAmount[_type][_token] - (payAmount[_type][_token] * discount[_type][_token]) / HUNDRED_PERCENT
-            );
-        }
+            uint256 usdAmount = paymentAmountInUSDT[_type] -
+                (paymentAmountInUSDT[_type] * discount[_type][address(0)]) /
+                HUNDRED_PERCENT;
+            uint256 requireETHAmount = getTokenAmountFromUSD(address(0), usdAmount);
 
-        emit Payment(msg.sender, _token, payAmount[_type][_token], discount[_type][_token], _type, _paymentId);
+            require(msg.value >= requireETHAmount, "Payment: not valid pay amount");
+
+            // cashback exceeds amount to sender
+            uint256 exceedETH = msg.value - requireETHAmount;
+            if (exceedETH > 0) {
+                payable(msg.sender).transfer(exceedETH);
+            }
+            emit Payment(msg.sender, _token, requireETHAmount, discount[_type][address(0)], _type, _paymentId);
+        } else {
+            uint256 usdAmount = paymentAmountInUSDT[_type] -
+                (paymentAmountInUSDT[_type] * discount[_type][_token]) /
+                HUNDRED_PERCENT;
+            uint256 requireTokenAmount = getTokenAmountFromUSD(address(0), usdAmount);
+
+            IERC20(_token).transferFrom(msg.sender, treasury, requireTokenAmount);
+            emit Payment(msg.sender, _token, requireTokenAmount, discount[_type][_token], _type, _paymentId);
+        }
     }
 
     function changeTreasury(address _treasury) external onlyOwner {
         treasury = _treasury;
         emit ChangeTreasury(_treasury);
+    }
+
+    function getTokenAmountFromUSD(address _token, uint256 _usdAmount) public view returns (uint256) {
+        (uint256 price, uint8 decimals) = getLatestPrice(_token);
+        return (_usdAmount * price) / decimals;
+    }
+
+    function getLatestPrice(address _token)
+        public
+        view
+        returns (
+            uint256,
+            uint8 /* decimals */
+        )
+    {
+        address priceFeed;
+
+        if (_token == address(0)) {
+            priceFeed = usdtEthPriceFeed;
+        } else if (_token == XCNToken) {
+            priceFeed = usdtXcnPriceFeed;
+        } else {
+            revert("Payment: invalid token");
+        }
+
+        (, int256 price, , , ) = AggregatorV3Interface(priceFeed).latestRoundData();
+        uint8 decimals = AggregatorV3Interface(priceFeed).decimals();
+        return (uint256(price), decimals);
     }
 }
